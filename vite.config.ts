b56c0b8bc177f765857d9be9088b68681c2aab12
sexxion <2068610832@qq.com>
2026-02-07 16:22:03 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vs', // 将 'vs' 替换为你的仓库名称
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        }
-      }
-    }
-  }
-})
+  base: "/vs/", // 将 'vs' 替换为你的仓库名称
+  // build: {
+  //   outDir: 'dist',
+  //   assetsDir: 'assets',
+  //   sourcemap: false,
+  //   minify: 'terser',
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: {
+  //         vendor: ['react', 'react-dom'],
+  //       }
+  //     }
+  //   }
+  // }
+});
